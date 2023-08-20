e12708f20af1116c106bd6c3bd282c769727537a
amirali72 <amirali531361@gmail.com>
2023-08-21 02:36:04 +0530

    add new

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="foot">
-        <p>Copyright &copy; iBlog.com</p>
+        <p>Copyright &copy; iBlog</p>
         <span>Made by Amir Ali</span>
     </footer>
   )
